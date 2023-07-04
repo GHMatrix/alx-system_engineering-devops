8046feb58572bc2cbfdd6408916427754ee2309c
GHMatrix <gsmensah@yahoo.com>
2023-07-04 09:02:37 -0700

    Text me

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,13 +1,4 @@
 #!/usr/bin/env rubyi
 #Regular expression
 
-i# Accept one argument from the command line
-input = ARGV[0]
-
-# Extract sender, receiver, and flags using regular expressions
-sender = input.match(/\[from:([^\]]+)\]/)&.captures&.first
-receiver = input.match(/\[to:([^\]]+)\]/)&.captures&.first
-flags = input.match(/\[flags:([^\]]+)\]/)&.captures&.first
-
-# Output the sender, receiver, and flags
-puts "#{sender},#{receiver},#{flags}"
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.?)\]/).join(",")
