b187fcda4f4ee8ff0b74d3b0506d62a0408acffd
GHMatrix <gsmensah@yahoo.com>
2023-07-05 02:50:01 -0700

    Repetition Token #0

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,17 +1,4 @@
 #!/usr/bin/env ruby
 #Regular expression
 
-# Script file: regex_match.rb
-
-# Accept one argument from the command line
-input = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /hbt{2,5}n/
-
-# Perform the regular expression matching
-if input.match?(pattern)
-  puts "Input matches the pattern!"
-else
-  puts "Input does not match the pattern."
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
