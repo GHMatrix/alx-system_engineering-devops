7e1cce9e6ee70f289ba0b8ceea3ba4f083f25b50
GHMatrix <gsmensah@yahoo.com>
2023-08-25 05:30:48 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# Puppet Manifest to Install a Package
+# Puppet Manifest to Install Flask 2.1.0 using pip3
+
+package { 'pip3':
+  ensure => 'installed',
+}
+
 package { 'flask':
-  ensure => '2.1.0',
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
