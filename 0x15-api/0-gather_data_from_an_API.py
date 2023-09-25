36bf7d5d1d6a7fd841a6e74727dce07b8dcf0963
GHMatrix <gsmensah@yahoo.com>
2023-09-25 12:45:13 +0000

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Python script that, using a REST API, retrieves and displays
+information about an employee's TODO list progress.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit("Usage: {} <employee_id>".format(sys.argv[0]))
+
+    employee_id = sys.argv[1]
+
+    # Get user info
+    user_info = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'
+            .format(employee_id))
+    user_data = user_info.json()
+    employee_name = user_data.get('name')
+
+    # Get user's tasks
+    user_tasks = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'
+            .format(employee_id))
+    tasks_data = user_tasks.json()
+
+    total_tasks = len(tasks_data)
+    completed_tasks = [task for task in tasks_data if task.get('completed')]
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, len(completed_tasks), total_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task.get('title')))
