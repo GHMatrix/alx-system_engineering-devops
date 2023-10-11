e7dc7e4eeb0c12b0d210fd44a7da73ab165c9968
GHMatrix <gsmensah@yahoo.com>
2023-10-11 16:46:02 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -9,8 +9,8 @@ class apache_fix {
 
   # Replacing "phpp" with "php" in wp-settings.php
   exec { 'modify_wp_settings':
-    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-    path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/',
     require => File['/var/www/html/wp-settings.php'],
   }
 
