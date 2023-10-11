d85af1e9ed31219f534f8d92a206a0c460a07148
GHMatrix <gsmensah@yahoo.com>
2023-10-11 17:07:33 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,6 @@
 # Puppet manifest to fix Apache 500 error
 
-class apache_fix {
-  # Ensuring  correct file exists
-  file { '/var/www/html/wp-settings.php':
-    ensure => file,
-    notify => Exec['modify_wp_settings'],
-  }
-
-  exec { 'fix-wordpress':
+exec { 'wordpress-fix':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
