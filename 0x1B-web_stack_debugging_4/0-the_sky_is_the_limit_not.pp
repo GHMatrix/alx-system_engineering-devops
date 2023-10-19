0cc70c6a217a55fb6835b02c6bd39f1cd48b29f3
GHMatrix <gsmensah@yahoo.com>
2023-10-19 12:00:29 +0000

    Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
-# Fix for failed requests till 0
+# Fix for failed requests
 exec { 'nginx-fix':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf && sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+}
